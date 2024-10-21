b5be415c2b977734ca9d9658f3ab53dfedf42908
Aida Koorang Beheshti <aida.beheshti81@gmail.com>
2024-10-21 14:30:27 +0330

    f

--- university/models.py
+++ university/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    student_id = models.IntegerField()
+    birth_date = models.DateField()
+
+    def __str__(self):
+        return self.name + " " + self.last_name
+
+
+class Course(models.Model):
+    pass
