21e229d6163d2ac1fe2f6be52ddc145effc30106
Aida Koorang Beheshti <aida.beheshti81@gmail.com>
2024-10-21 15:04:37 +0330

    f

--- university/views.py
+++ university/views.py
@@ -1,9 +1,14 @@
-from rest_framework import generics
+from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Student
 from .serializers import StudentSerializer
+from .filter_backends import StudentFilter
 
 
 class StudentListCreateAPIView(generics.ListCreateAPIView):
-    serializer_class = StudentSerializer
     queryset = Student.objects.all()
-
+    serializer_class = StudentSerializer
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
+    filterset_class = StudentFilter
+    search_fields = ['name', 'last_name'] 
+    ordering_fields = ['student_id', 'name', 'birth_date']
