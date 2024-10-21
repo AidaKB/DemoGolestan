b1e45f20e5eef2500ca5dc03df8bd5d868fa839a
fatemeh <fatemehsadatmousavi1382@gmail.com>
2024-10-21 15:03:14 +0330

    tables added

--- university/models.py
+++ university/models.py
@@ -19,3 +19,21 @@ class Course(models.Model):
     semester = models.CharField(max_length=255)
     credit = models.IntegerField()
 
+
+# add a 'takes' table to represent the many-to-many relationship between students and courses
+class Takes(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    grade = models.IntegerField()
+    semester = models.CharField(max_length=255)
+    year = models.IntegerField()
+
+
+# add a 'teachers' table to represent teachers
+class Teacher(models.Model):
+    name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    teacher_id = models.IntegerField()
+
+    def __str__(self):
+        return self.name + " " + self.last_name
